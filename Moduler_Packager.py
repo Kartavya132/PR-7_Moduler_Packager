0f4e5a07bebc7dbbed75645dbccc156a4bbdb47a
Kartavya Patel <kartavyapatel630@gmail.com>
2026-06-18 19:07:51 +0530

    Adding menu function

--- Moduler_Packager.py
+++ Moduler_Packager.py
@@ -5,11 +5,23 @@ import uuid
 from sys import exit
 
 
+def menu_build(*arg):
+    menu = arg
+    print(f"-----{menu[0]}-----")
+    try:
+        for i in range(1, len(menu)):
+            print(f"{i}. {menu[i]}")
+        print("-" * (10 + len(menu[0])))
+        return input("Enter your choice : ")
+    except:
+        print("Enter the choice proper")
+
+
 def date_time():
-    pass
+    choi = menu_build("Date-Time Menu")
 
 
-def mathematis():
+def mathematic():
     pass
 
 
@@ -30,7 +42,42 @@ def attributes():
 
 
 def show_menu():
-    pass
+    while True:
+        print("------------------------------")
+        print("| Welcome to omni-media tool |")
+        print("------------------------------\n")
+
+        print("------Main Menu------")
+        print("1. Datetime and Time Operations")
+        print("2. Mathematical  Operations")
+        print("3. Random Data Generator")
+        print("4. Generate Unique id(UUID)")
+        print("5. File Operations")
+        print("6. Attributes explore")
+        print("7. Exit")
+        print("---------------------")
+        choice = input("Enter your choice : ")
+
+        match choice:
+            case "1":
+                date_time()
+            case "2":
+                mathematic()
+            case "3":
+                random_data()
+            case "4":
+                unique_id()
+            case "5":
+                files()
+            case "6":
+                attributes()
+            case "7":
+                print("Thank for using me!!")
+                print("Good Bye")
+                exit()
+            case _:
+                print("Invalid choice")
+                print("enter the choice as per instruction only")
 
 
 def main():
