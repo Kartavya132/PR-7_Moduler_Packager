87b17800742fce8e66ad9143dfe0dcd650f23f33
Kartavya Patel <kartavyapatel630@gmail.com>
2026-06-18 18:48:19 +0530

    Added all the blue prints

---
+++ Moduler_Packager.py
@@ -0,0 +1,41 @@
+import math
+import datetime
+import random
+import uuid
+from sys import exit
+
+
+def date_time():
+    pass
+
+
+def mathematis():
+    pass
+
+
+def random_data():
+    pass
+
+
+def unique_id():
+    pass
+
+
+def files():
+    pass
+
+
+def attributes():
+    pass
+
+
+def show_menu():
+    pass
+
+
+def main():
+    show_menu()
+
+
+if __name__ == "__main__":
+    main()
