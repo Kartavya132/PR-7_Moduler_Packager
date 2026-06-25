7c39ec614e21a32717a6683eda0cff09e1fcbe82
Kartavya Patel <kartavyapatel630@gmail.com>
2026-06-25 09:48:51 +0530

    Import various tools in __init__.py

--- Package/__init__.py
+++ Package/__init__.py
@@ -1,1 +1,6 @@
-__version__ = '1.0.0'
+from .datetime_tools import date_time
+from .math_tools import mathematic
+from .random_tools import random_data
+from .uuid_tools import unique_id
+from .file_tools import files
+from .attributes import attributes
