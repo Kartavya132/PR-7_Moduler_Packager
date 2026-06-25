b75daa27ca8b734d0cae4798200d3eed81148462
Kartavya Patel <kartavyapatel630@gmail.com>
2026-06-25 09:54:30 +0530

    Add unique_id function to uuid_tools.py

--- Package/uuid_tools.py
+++ Package/uuid_tools.py
@@ -1,1 +1,8 @@
-# Move unique_id() here.
+import uuid
+
+def unique_id():
+    print("Generate Unique ID ::-")
+    my_uuid = uuid.uuid4()
+    print(f"ID:- {my_uuid}")
+    print("Thank you!, Press Enter to continue")
+    input()
