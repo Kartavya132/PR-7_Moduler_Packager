701426946c4d43019ab73fb16469ff98b7d0b01d
Kartavya Patel <kartavyapatel630@gmail.com>
2026-06-25 09:46:37 +0530

    Fix typos in menu and exit messages

--- Package/main.py
+++ Package/main.py
@@ -1,3 +1,13 @@
+import sys
+from omni_utils import (
+    date_time,
+    mathematic,
+    random_data,
+    unique_id,
+    files,
+    attributes
+)
+
 def show_menu():
     while True:
         print("------------------------------")
@@ -6,7 +16,7 @@ def show_menu():
 
         print("------Main Menu------")
         print("1. Datetime and Time Operations")
-        print("2. Mathematical  Operations")
+        print("2. Mathematical Operations")
         print("3. Random Data Generator")
         print("4. Generate Unique id(UUID)")
         print("5. File Operations")
@@ -29,7 +39,7 @@ def show_menu():
             case "6":
                 attributes()
             case "7":
-                print("Thank for using me!!")
+                print("Thanks for using me!!")
                 print("Good Bye")
                 sys.exit()
             case _:
@@ -37,10 +47,8 @@ def show_menu():
                 print("Enter the choice as per instruction only")
         print("====================")
 
-
 def main():
     show_menu()
-
 
 if __name__ == "__main__":
     main()
