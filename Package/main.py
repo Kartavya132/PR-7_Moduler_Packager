ab7c44216161e735ed7fa66685ec094afcf0ee30
Kartavya Patel <kartavyapatel630@gmail.com>
2026-06-25 09:42:27 +0530

    Implement main menu and entry point functions

--- Package/main.py
+++ Package/main.py
@@ -1,1 +1,46 @@
-# Entry point. Move show_menu() and main() here.
+def show_menu():
+    while True:
+        print("------------------------------")
+        print("| Welcome to omni-media tool |")
+        print("------------------------------\n")
+
+        print("------Main Menu------")
+        print("1. Datetime and Time Operations")
+        print("2. Mathematical  Operations")
+        print("3. Random Data Generator")
+        print("4. Generate Unique id(UUID)")
+        print("5. File Operations")
+        print("6. Attributes explore")
+        print("7. Exit")
+        print("---------------------")
+        choice = input("Enter your choice : ")
+
+        match choice:
+            case "1":
+                date_time()
+            case "2":
+                mathematic()
+            case "3":
+                random_data()
+            case "4":
+                unique_id()
+            case "5":
+                files()
+            case "6":
+                attributes()
+            case "7":
+                print("Thank for using me!!")
+                print("Good Bye")
+                sys.exit()
+            case _:
+                print("Invalid choice")
+                print("Enter the choice as per instruction only")
+        print("====================")
+
+
+def main():
+    show_menu()
+
+
+if __name__ == "__main__":
+    main()
